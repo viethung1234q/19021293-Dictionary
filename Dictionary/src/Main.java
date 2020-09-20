ed29b7a02e3d69827361496162e3e9c09c64fe34
viethung1234q <71419315+viethung1234q@users.noreply.github.com>
2020-09-21 00:13:55 +0700

    Update Main.java

--- Dictionary/src/Main.java
+++ Dictionary/src/Main.java
@@ -11,29 +11,5 @@ public class Main {
         DictionaryCommandline cmd = new DictionaryCommandline();
         cmd.insertFromFile();
         cmd.showAllWords();
-
-        /*try {
-            Formatter f = new Formatter("myFile");
-            f.format("day la file mau");
-            f.format("%s %s %s%n", "1","John","Smith");
-            f.format("%s %s %s%n", "2","Amy","Brown");
-            f.close();
-        } catch (FileNotFoundException e) {
-            System.out.print("Error");
-        }*/
-
-        /*try {
-            File x = new File("myFile");
-            Scanner sc = new Scanner(x);
-            //StringBuilder content = new StringBuilder();
-            while(sc.hasNextLine()) {
-                System.out.print(sc.nextLine() + "\n");
-            }
-            //System.out.println(content);
-            sc.close();
-        } catch (FileNotFoundException e) {
-            System.out.print("Error");
-        }*/
-
     }
 }
