f823152dfcdbcff0f40b6849660bdb87c4df0ae4
viethung1234q <71419315+viethung1234q@users.noreply.github.com>
2020-09-22 22:14:41 +0700

    Update Word.java

--- Dictionary/src/Word.java
+++ Dictionary/src/Word.java
@@ -7,12 +7,17 @@ public class Word {
         word_explain = "";
     }
 
+    Word(Word newWord) {
+        this.word_target = newWord.getWord_target();
+        this.word_explain = newWord.getWord_explain();
+    }
+
     Word(String word_target, String word_explain) {
         this.word_target = word_target;
         this.word_explain = word_explain;
     }
 
-    // word_target's getter/setter
+    // Word_target's getter/setter
     public void setWord_target(String word_target) {
         this.word_target = word_target;
     }
@@ -21,12 +26,17 @@ public class Word {
         return word_target;
     }
 
-    // word_explain's getter/setter
+    // Word_explain's getter/setter
     public void setWord_explain(String word_explain) {
         this.word_explain = word_explain;
     }
 
     public String getWord_explain() {
         return word_explain;
+    }
+    
+    // Compare 2 word to sort them in order
+    public int compareTo(Word other) {
+        return (this.word_target.compareTo(other.word_target));
     }
 }
