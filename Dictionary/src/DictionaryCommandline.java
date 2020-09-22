0505b102260b1fdc0852583e4574531a2848711e
viethung1234q <71419315+viethung1234q@users.noreply.github.com>
2020-09-22 22:11:14 +0700

    Update DictionaryCommandline.java

--- Dictionary/src/DictionaryCommandline.java
+++ Dictionary/src/DictionaryCommandline.java
@@ -1,13 +1,38 @@
+import java.util.Scanner;
+
 public class DictionaryCommandline extends DictionaryManagement {
     public void showAllWords() {
         System.out.printf("%-4s|%-15s|%-15s%n","No", "Word", "Meaning");
-        for (int i = 0; i < myDictionary.getWordCount(); i++) {
-            System.out.printf("%-4d|%-15s|%-15s%n", i + 1, myDictionary.getWords()[i].getWord_target(),
-                                                           myDictionary.getWords()[i].getWord_explain());
+        int i = 1;
+        for (Word item : myDictionary.words) {
+            if (i <= myDictionary.words.size()) {
+                System.out.printf("%-4d|%-15s|%-15s%n", i, item.getWord_target(), item.getWord_explain());
+                i++;
+            }
         }
     }
+
+    public void dictionarySearcher() {
+        System.out.print("- Enter word you want to search: ");
+        Scanner sc = new Scanner(System.in);
+        String searchWord = sc.nextLine();
+        int length = searchWord.length();
+
+        for (Word item : myDictionary.words) {
+            if (searchWord.equals(item.getWord_target().substring(0, length))) {
+                System.out.println(item.getWord_target());
+            }
+        }
+    }
+
     public void dictionaryBasic() {
         insertFromCommandline();
         showAllWords();
+    }
+
+    public void dictionaryAdvanced() {
+        insertFromCommandline();
+        showAllWords();
+        dictionaryLookup();
     }
 }
