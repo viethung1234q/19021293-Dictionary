42d7cfcdf6c0b0a260efc954b53419f80ef52346
viethung1234q <71419315+viethung1234q@users.noreply.github.com>
2020-09-22 22:10:53 +0700

    Update Dictionary.java

--- Dictionary/src/Dictionary.java
+++ Dictionary/src/Dictionary.java
@@ -1,28 +1,5 @@
+import java.util.TreeSet;
+
 public class Dictionary {
-    private Word[] words;
-    private int wordCount;
-
-    public Dictionary() {
-        this.words = new Word[100];
-        this.wordCount = 0;
-    }
-
-    public void addWord(Word newWord) {
-        words[wordCount] = newWord;
-        wordCount++;
-    }
-
-    public int getWordCount() {
-        return wordCount;
-    }
-    public void setWordCount(int wordCount) {
-        this.wordCount = wordCount;
-    }
-
-    public Word[] getWords() {
-        return words;
-    }
-    public void setWords(Word[] words) {
-        this.words = words;
-    }
+    TreeSet<Word> words = new TreeSet<>(Word::compareTo);
 }
